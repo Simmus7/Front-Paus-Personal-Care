a3824c98dcde8ed4705b698cb5e752e4bed918d9
Simmus7 <64326113+Simmus7@users.noreply.github.com>
2021-10-21 14:21:29 -0500

    DetailsViewProgress

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -12,46 +12,54 @@ export default class Details extends Component {
       products: [],
       productToShow: null
     }
+
+    this.getProducts=this.getProducts.bind(this)
   }
 
-  componentDidMount () {
+  async getProducts (){
+    const res = await axios.get('https://backend-paus-personal-care.herokuapp.com/products/get/') //ESto si esta corriendo?
+
+    this.setState({
+      products: res.data
+    })
     
-    axios.get('http://localhost:5000/products/get/').then(
-      response => {
-        this.setState({
-          products: response.data
-          
-        })
-        
+    console.log("PRODUCTOS DETALLE")
+    console.log(res.data)
+
+    //var products = [];
+    const { match: { params } } = this.props;
+    const idproduct = params.id;
+    var productsaux;
+  
+    //products[0]={"_id":"615bd31a191e4b60cf285747","productname":"Desodorante","description":"Para el mal olor ","price":500,"productImage":["https://res.cloudinary.com/pauspersonalcare/image/upload/v1633407766/pauspersonalcare/xqm2tyine0i0ulirocbb.jpg"],"createdAt":"2021-10-05T04:22:50.172Z","updatedAt":"2021-10-05T04:22:50.172Z","__v":0};
+    /*this.state.products.map((product) => {
+      if(product._id === idproduct){
+          productsaux = product;
       }
-      )
-      .catch (() => console.log('NO TRAJO LOS PRODUCTOS'));
-      //var products = [];
-      const { match: { params } } = this.props;
-      const idproduct = params.id;
-      var productsaux;
-    
-        //products[0]={"_id":"615bd31a191e4b60cf285747","productname":"Desodorante","description":"Para el mal olor ","price":500,"productImage":["https://res.cloudinary.com/pauspersonalcare/image/upload/v1633407766/pauspersonalcare/xqm2tyine0i0ulirocbb.jpg"],"createdAt":"2021-10-05T04:22:50.172Z","updatedAt":"2021-10-05T04:22:50.172Z","__v":0};
-        this.state.products.map((product) => {
-          if(product._id === idproduct){
-              productsaux = product;
-          }
-        })
-        this.setState({
-          productToShow: productsaux          
-        })
-    };
+    })*/
 
-    render () {
-      
+    res.data.map((product) => {
+      if(product._id === idproduct){
+          productsaux = product;
+      }
+    })
 
-    return(     
-    <>        
-    <Detalles product = {this.state.productToShow} />
-    </>
-     )
+      this.setState({
+        productToShow: productsaux          
+      })
+  
 
-    };
+
 
 }
+render () {
+  this.getProducts();
+  return(     
+  <>        
+  <Detalles product = {this.state.productToShow} />
+  </>
+   )
 
+  }
+
+}
